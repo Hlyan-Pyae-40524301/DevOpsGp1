64e2668f30b6875f39bfe12dc634190ca3e2acb1
Peter2372001 <40516579@live.napier.ac.uk>
2023-07-27 18:35:56 +0630

    Add Integration Testing for Top Populated Countries In Continent Reports by Population

--- src/test/java/com/group1/cw/AppIntegrationTest.java
+++ src/test/java/com/group1/cw/AppIntegrationTest.java
@@ -75,4 +75,18 @@ public class AppIntegrationTest
         assertEquals(cou.get(0).Region, "Eastern Asia");
         assertEquals(cou.get(0).Population, 1277558000);
     }
+
+    // Integration Testing for Top Populated Countries In Continent Report by Population
+    @Test
+    void getTopPopulatedCountriesInContinent()
+    {
+        ArrayList<Country> cou = new ArrayList<>();
+        cou = app.getTopPopulatedCountriesInContinent();
+        assertEquals(cou.get(0).Capital, "Peking");
+        assertEquals(cou.get(0).Code, "CHN");
+        assertEquals(cou.get(0).Name, "China");
+        assertEquals(cou.get(0).Continent, "Asia");
+        assertEquals(cou.get(0).Region, "Eastern Asia");
+        assertEquals(cou.get(0).Population, 1277558000);
+    }
 }
